363998694e69511c59b26f68b2e00ec094a7253a
Michael Babcock <mjetpax@gmail.com>
2023-10-18 17:35:02 -0700

    add: initial commit of project

---
+++ Mentorship_CalorieTracker/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mentorship_CalorieTracker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
