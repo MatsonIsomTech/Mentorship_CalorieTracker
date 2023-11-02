ed5a9f4532d361c626facb49b5444d79f2dc6bfb
Michael Babcock <mjetpax@gmail.com>
2023-11-02 15:01:55 -0700

    update: journal listing

--- Mentorship_CalorieTracker/Pages/Journal.cshtml.cs
+++ Mentorship_CalorieTracker/Pages/Journal.cshtml.cs
@@ -3,21 +3,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Mentorship_CalorieTracker.Data;
 using Mentorship_CalorieTracker.Models;
 using Mentorship_CalorieTracker.Pages;
+using Mentorship_CalorieTracker.Data.Migrations;
 
-public class JournalModel : PageModel
+namespace Mentorship_CalorieTracker.Pages
 {
-    private readonly ILogger<JournalEntryModel> _logger;
-    private readonly ApplicationDbContext _context;
-
-    public JournalModel(ILogger<JournalEntryModel> logger, ApplicationDbContext context)
+    public class JournalModel : PageModel
     {
-        _logger = logger;
-        _context = context;
-    }
+        private readonly ILogger<JournalModel> _logger;
+        private readonly ApplicationDbContext _context;
+        public IList<Entry> Entries { get; set; } = new List<Entry>();
 
-    public IActionResult OnGet()
-    {
-        IList<Entry> entries = _context.Entries.OrderByDescending(e => e.EntryDate).ToList();
-        return Page();
+        public JournalModel(ILogger<JournalModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            Entries = _context.Entries.OrderByDescending(e => e.EntryDate).ToList();
+            return Page();
+        }
     }
 }
