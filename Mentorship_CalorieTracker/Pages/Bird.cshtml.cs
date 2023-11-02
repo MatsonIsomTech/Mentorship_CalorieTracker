84a1a7519dd2ce490304d600ac6991bb0dd0e3fa
Michael Babcock <mjetpax@gmail.com>
2023-11-02 16:06:20 -0700

    add: bird page

---
+++ Mentorship_CalorieTracker/Pages/Bird.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mentorship_CalorieTracker.Pages
+{
+    public class BirdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
